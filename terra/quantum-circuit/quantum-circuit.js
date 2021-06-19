8abb3ce720b3e78790188f38248f6533bddd6b77
Louis Lefevre <louislefev@gmail.com>
2021-06-19 03:46:58 +0100

    Add python shell execution
    Can now execute a python script on node activation.

--- terra/quantum-circuit/quantum-circuit.js
+++ terra/quantum-circuit/quantum-circuit.js
@@ -3,7 +3,23 @@ module.exports = function(RED) {
 
   function QuantumCircuitNode(config) {
     RED.nodes.createNode(this, config);
+    this.qubits = config.qubits;
+    this.cbits = config.cbits;
     const node = this;
+    const pythonShell = require('python-shell').PythonShell;
+
+    this.on('input', function(msg) {
+      const options = {
+        scriptPath: __dirname,
+        args: [node.qubits, node.cbits],
+      };
+
+      pythonShell.run('quantum-circuit.py', options, function(err, result) {
+        if (err) throw err;
+        msg.payload = result;
+        node.send(msg);
+      });
+    });
   }
 
   RED.nodes.registerType('quantum-circuit', QuantumCircuitNode);
---
+++ terra/quantum-circuit/quantum-circuit.py
@@ -0,0 +1,7 @@
+import sys
+
+qubits = int(sys.argv[1])
+cbits = int(sys.argv[2])
+
+print(f'Qubits: {qubits}')
+print(f'Classical Bits: {cbits}')
