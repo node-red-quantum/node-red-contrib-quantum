a3b172dff39a96e8d5d85d4b213ed03f0e0504bc
Louis Lefevre <louislefev@gmail.com>
2021-06-21 01:08:16 +0100

    Run Python scripts using venv path
    Python scripts can now be run via the virtual environment.

--- terra/quantum-circuit/quantum-circuit.js
+++ terra/quantum-circuit/quantum-circuit.js
@@ -1,15 +1,25 @@
 module.exports = function(RED) {
   'use strict';
 
+  const path = require('path');
+  const appRoot = require('app-root-path').path;
+  const pythonShell = require('python-shell').PythonShell;
+  const fs = require('fs');
+  const pythonPath = path.resolve(appRoot, 'venv/bin/python');
+
+  if (!fs.existsSync(pythonPath)) {
+    console.log('WARNING: venv/bin/python does not exist - execute the "npm run setup" command');
+  }
+
   function QuantumCircuitNode(config) {
     RED.nodes.createNode(this, config);
     this.qubits = config.qubits;
     this.cbits = config.cbits;
     const node = this;
-    const pythonShell = require('python-shell').PythonShell;
 
     this.on('input', function(msg) {
       const options = {
+        pythonPath: pythonPath,
         scriptPath: __dirname,
         args: [node.qubits, node.cbits],
       };
