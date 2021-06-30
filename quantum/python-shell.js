f6fe9136694047d0ee189ab4e2573672e428ee89
Louis Lefevre <louislefev@gmail.com>
2021-06-30 15:12:08 +0100

    Add Python shell execution
    Functionality for executing Python code via a Python shell process.
    
    Still a work in progress - this commit is not without issues.

--- quantum/python-shell.js
+++ quantum/python-shell.js
@@ -1,17 +1,59 @@
+'use strict';
+
 const path = require('path');
 const appRoot = require('app-root-path').path;
 const fileSystem = require('fs');
 const pythonShell = require('python-shell').PythonShell;
 const pythonPath = path.resolve(appRoot, 'venv/bin/python');
+const childProcess = require('child_process');
+const pythonProcess = childProcess.spawn(pythonPath, ['-i']);
+pythonProcess.stdout.setEncoding('utf8');
+pythonProcess.stderr.setEncoding('utf8');
+
 
 /**
- * Runs a Python script and returns collected messages
+ * Writes to stdin for the Python process.
+ * @param {string} input Code to execute within the Python shell
+*/
+function writeInput(input) {
+  pythonProcess.stdin.write(input+'\n');
+}
+
+
+/**
+ * Reads stdout from the Python process.
+ * @param {Function} callback Callback function to invoke on the output message
+*/
+function readOutput(callback) {
+  pythonProcess.stdout.once('data', function(data) {
+    callback(data);
+  });
+}
+
+
+/**
+ * Reads stderr from the Python process.
+ * @param {Function} callback Callback function to invoke on the error message
+*/
+function readError(callback) {
+  pythonProcess.stderr.once('data', function(data) {
+    if (data.startsWith('Python 3.') || (data.startsWith('...')) || data.startsWith('>>>')) {
+      callback(null);
+    } else {
+      callback(data);
+    }
+  });
+}
+
+
+/**
+ * Runs a Python script and returns collected messages.
  * @param {string}   scriptPath Directory path to the script (excludes file name)
  * @param {string}   scriptName File name of the script
  * @param {string[]} args       Arguments to pass to the script
  * @param {Function} callback   Callback function to invoke with the script results
 */
-module.exports = function(scriptPath, scriptName, args, callback) {
+function runScript(scriptPath, scriptName, args, callback) {
   if (!fileSystem.existsSync(pythonPath)) {
     throw new Error('cannot resolve Python virtual environment - execute the "npm run setup" command');
   }
@@ -21,5 +63,14 @@ module.exports = function(scriptPath, scriptName, args, callback) {
     scriptPath: scriptPath,
     args: args,
   };
+
   pythonShell.run(scriptName, options, callback);
+};
+
+
+module.exports = {
+  writeInput,
+  readOutput,
+  readError,
+  runScript,
 };
