6e11e2b17fda0c679fdef6cf9522db97f4f39225
Louis Lefevre <louislefev@gmail.com>
2021-07-02 05:46:11 +0100

    Python shell process testing

--- quantum/terra/classical-register/classical-register.js
+++ quantum/terra/classical-register/classical-register.js
@@ -1,9 +1,28 @@
 module.exports = function(RED) {
   'use strict';
 
+  const newProcess = require('../../python-shell');
+
   function ClassicalRegisterNode(config) {
     RED.nodes.createNode(this, config);
     const node = this;
+
+    this.on('input', function(msg) {
+      newProcess.readError(function(err) {
+        if (err != null) node.error(err);
+        return null;
+      });
+
+      newProcess.readOutput(function(data) {
+        msg.payload = data;
+        node.send(msg);
+      });
+
+      newProcess.writeInput('job = simulator.run(compiled_circuit, shots=1000)');
+      newProcess.writeInput('result = job.result()');
+      newProcess.writeInput('counts = result.get_counts(circuit)');
+      newProcess.writeInput('print("Total count for 00 and 11 are:", counts)');
+    });
   }
 
   RED.nodes.registerType('classical-register', ClassicalRegisterNode);
--- quantum/terra/quantum-circuit/quantum-circuit.js
+++ quantum/terra/quantum-circuit/quantum-circuit.js
@@ -1,7 +1,7 @@
 module.exports = function(RED) {
   'use strict';
 
-  const runShell = require('../../python-shell');
+  const newProcess = require('../../python-shell');
 
   function QuantumCircuitNode(config) {
     RED.nodes.createNode(this, config);
@@ -9,12 +9,27 @@ module.exports = function(RED) {
     this.cbits = config.cbits;
     const node = this;
 
-    this.on('input', function(msg) {
-      runShell(__dirname, 'quantum-circuit.py', [node.qubits, node.cbits], function(err, output) {
-        if (err) throw err;
-        msg.payload = output;
+    this.on('input', function(msg, send, done) {
+      newProcess.readError(function(err) {
+        if (err != null) node.error(err);
+        return null;
+      });
+
+      newProcess.readOutput(function(data) {
+        msg.payload = data;
         node.send(msg);
       });
+
+      newProcess.writeInput('from qiskit import QuantumCircuit, transpile');
+      newProcess.writeInput('from qiskit.providers.aer import QasmSimulator');
+      newProcess.writeInput('from qiskit.visualization import plot_histogram');
+      newProcess.writeInput('simulator = QasmSimulator()');
+      newProcess.writeInput('circuit = QuantumCircuit(2, 2)');
+      newProcess.writeInput('_ = circuit.h(0)');
+      newProcess.writeInput('_ = circuit.cx(0, 1)');
+      newProcess.writeInput('_ = circuit.measure([0,1], [0,1])');
+      newProcess.writeInput('compiled_circuit = transpile(circuit, simulator)');
+      newProcess.writeInput('print("Done 1")');
     });
   }
 
