92288e210067f5870231f04e3a433570bbb677dd
Robin Zhou <bbaayezi@gmail.com>
2021-07-05 23:46:45 +0100

    init hadamard gate

---
+++ quantum/terra/hadamard-gate/hadamard-gate.js
@@ -0,0 +1,10 @@
+module.exports = function (RED) {
+    'use strict';
+
+    function NodeTemplateNode(config) {
+        RED.nodes.createNode(this, config);
+        const node = this;
+    }
+
+    RED.nodes.registerType('node-template', NodeTemplateNode);
+};
