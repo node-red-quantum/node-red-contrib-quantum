a3c6a2ec187cca7a2b27a3715053e1d72ac27217
Theo Reignier <theo.reignier.99@gmail.com>
2021-07-05 15:43:37 +0200

    Working version of initialisation nodes

--- quantum/terra/quantum-circuit/quantum-circuit.js
+++ quantum/terra/quantum-circuit/quantum-circuit.js
@@ -1,20 +1,44 @@
 module.exports = function(RED) {
   'use strict';
 
-  const runShell = require('../../python-shell');
-
   function QuantumCircuitNode(config) {
+
+    // Creating node with properties and context 
     RED.nodes.createNode(this, config);
-    this.qubits = config.qubits;
-    this.cbits = config.cbits;
+    this.outputs = config.outputs;
+    const globalContext = this.context().global;
     const node = this;
 
-    this.on('input', function(msg) {
-      runShell(__dirname, 'quantum-circuit.py', [node.qubits, node.cbits], function(err, output) {
-        if (err) throw err;
-        msg.payload = output;
-        node.send(msg);
-      });
+    this.on('input', function(msg, send, done) {
+
+      // Storing import script to the 'script' global variable
+      const qiskitScript = (
+        "import numpy as np \n" +
+        "import qiskit \n" +
+        "from qiskit import * \n"
+      );
+      globalContext.set("script", qiskitScript);
+
+      // Creating an empty 'structure' global array
+      // This variable represents the quantum circuit structure
+      const structure = new Array(this.outputs);
+      globalContext.set("structure", structure);
+      
+      // Creating an array of messages to be sent
+      // Each message represents a dfifferent register
+      var i = 0;
+      var output = new Array(this.outputs);
+      for (i=0; i<this.outputs; i++){
+        output[i] = {
+          topic: "Quantum Circuit",
+          payload: {
+            register: i
+          }
+        }
+      };
+
+      // Sending one register object per node output
+      send(output);
     });
   }
 
--- quantum/terra/quantum-register/quantum-register.js
+++ quantum/terra/quantum-register/quantum-register.js
@@ -2,8 +2,79 @@ module.exports = function(RED) {
   'use strict';
 
   function QuantumRegisterNode(config) {
+
+    // Creating node with properties and context 
     RED.nodes.createNode(this, config);
+    this.outputs = config.outputs;
+    const globalContext = this.context().global;
     const node = this;
+
+    this.on('input', function(msg, send, done) {
+
+      // Appending Qiskit script to the 'script' global variable
+      var qiskitScript = (
+        "\nqr" + msg.payload.register.toString() + 
+        " = QuantumRegister(" + this.outputs.toString() + 
+        ", '" + (this.name || ("R" + msg.payload.register.toString())) + "')"
+      );
+      var oldScript = globalContext.get("script");
+      globalContext.set("script", oldScript + qiskitScript);
+
+      // Completing the 'structure' global array 
+      var structure = globalContext.get("structure");
+      structure[msg.payload.register] = {
+        registerType: "quantum",
+        registerName: (this.name || ("R" + msg.payload.register.toString())),
+        registerVar: "qr" + msg.payload.register.toString(),
+        bits: this.outputs
+      };
+      globalContext.set("structure", structure);
+      
+      // Counting the number of registers that were set in the 'structure' array
+      var count = 0;
+      structure.map(x => {
+        if (typeof(x) !== "undefined"){
+          count += 1
+        }
+      });
+
+      // If they are all set: initialise the quantum circuit
+      if (count == structure.length) {
+
+        // Generating the corresponding Qiskit script
+        qiskitScript = "\n \nqc = QuantumCircuit(";
+        structure.map(register => {
+          if (register.registerType === "quantum") {
+            qiskitScript += ("qr" + structure.indexOf(register) + ",");
+          } else {
+            qiskitScript += ("cr" + structure.indexOf(register) + ",");
+          }
+        })
+        qiskitScript = qiskitScript.substring(0,qiskitScript.length - 1);
+        qiskitScript += ") \n";
+        
+        // Appending the code to the 'script' global variable
+        oldScript = globalContext.get("script");
+        globalContext.set("script", oldScript + qiskitScript);
+      }
+
+      // Creating an array of messages to be sent 
+      // Each message represents a different qubit
+      var i = 0;
+      var output = new Array(this.outputs);
+      for (i=0; i<this.outputs; i++){
+        output[i] = {
+          topic: "Quantum Circuit",
+          payload: {
+            register: "qr" + msg.payload.register.toString(),
+            qubit: i
+          }
+        }
+      };
+      
+      // Sending one qubit object per node output
+      send(output);
+    });
   }
 
   RED.nodes.registerType('quantum-register', QuantumRegisterNode);
