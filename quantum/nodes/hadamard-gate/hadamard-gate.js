738e46f90baf8a4f0bec1675e4314856f517833e
Robin Zhou <bbaayezi@gmail.com>
2021-07-08 01:35:49 +0100

    add hadmard gate

--- quantum/nodes/hadamard-gate/hadamard-gate.js
+++ quantum/nodes/hadamard-gate/hadamard-gate.js
@@ -1,11 +1,25 @@
 module.exports = function(RED) {
   'use strict';
 
-  function NodeTemplateNode(config) {
+  function HadamardGate(config) {
     RED.nodes.createNode(this, config);
+    const globalContext = this.context().global;
     const node = this;
+
+    node.on('input', (msg, send, done) => {
+      let qrConfig = msg.payload;
+      let keys = Object.keys(qrConfig);
+      if (!keys.includes('register') || !keys.includes('qubit')) {
+        throw new Error('Invalid Input');
+      }
+      let newScript = `\nqc.h(${qrConfig.register}[${qrConfig.qubit}])`;
+      let oldScript = globalContext.get('script');
+      globalContext.set('script', oldScript + newScript);
+      // pass the quantum register config to the output
+      send(msg);
+    })
   }
 
-  RED.nodes.registerType('node-template', NodeTemplateNode);
+  RED.nodes.registerType('hadamard-gate', HadamardGate);
 };
   
