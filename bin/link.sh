3cea4036a492b50a005ccad55b1673bdb32f6525
Louis Lefevre <louislefev@gmail.com>
2021-07-11 14:30:01 +0100

    Fix space escaping for paths
    If a path has whitespace, it was being split into multiple arguments.
    This update ensures that all variables aren't accidentally split.

--- bin/link.sh
+++ bin/link.sh
@@ -13,8 +13,8 @@ fi
 
 # Set path variables.
 package="node-red-contrib-quantum"
-repo_path=$PWD
-red_path=$HOME/.node-red
+repo_path="$PWD"
+red_path="$HOME/.node-red"
 
 # Install dependencies for package.
 if npm install; then
@@ -25,20 +25,20 @@ else
 fi
 
 # Check if Node-RED user directory exists. If no, create it.
-if [ ! -d $red_path ]; then
+if [ ! -d "$red_path" ]; then
   echo "Creating Node RED user directory at $red_path"
-  mkdir $red_path
+  mkdir "$red_path"
 else
   echo "Using Node-RED user directory at $red_path"
 fi
 
 # Change directory into Node-RED user directory
-cd $red_path
+cd "$red_path"
 
 # Check if package is already installed. If no, install it.
-if [[ ! $(npm list -p | grep $package) ]]; then
+if [[ ! $(npm list -p | grep "$package") ]]; then
   echo "Installing $package"
-  if npm install $repo_path; then
+  if npm install "$repo_path"; then
     echo "Successfully linked $package with Node-RED"
   else
     echo "Failed to link $package with Node-RED"
