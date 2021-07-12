0ae9624d8757f617d3ea65332cef2d0e7fa10d9e
Khesim Reid <khesim.reid@gmail.com>
2021-07-12 12:39:39 +0100

    Add code to get qubits in dropdown box.

--- quantum/nodes/toffoli-gate/toffoli-gate.js
+++ quantum/nodes/toffoli-gate/toffoli-gate.js
@@ -2,45 +2,26 @@ module.exports = function (RED) {
   "use strict";
 
   function ToffoliGateNode(config) {
-    // Creating node with properties and context
     RED.nodes.createNode(this, config);
-    this.name = config.name;
-    this.outputs = config.outputs;
-    const globalContext = this.context().global;
     const node = this;
+    this.outputs = config.outputs;
+    this.controlQubit1 = config.controlQubit1;
+    this.controlQubit2 = config.controlQubit2;
+    this.targetQubit = config.targetQubit;
+    const globalContext = this.context().global;
     const output = new Array(node.outputs);
 
-    //Connect gate to 3 quantum registers as input, indicating which input are the controls and which is the target
-    //Add the inputs to the qiskit code ' qc = ccx(qr1, qr2, qr3)' and append to the script
-    //Output qr1, qr2 and qr3 from gate.
+    //Access the message payload to get the 3 qubit inputs.
+    //Wait until the gate has all 3 qubits
+    //Apply qiskit code 'cxx(Control-q1, Control-q2, Target-q3)
+    //Output the three qubits
 
-    this.on("input", function (msg, send, done) {
-      // Appending Qiskit script to the 'script' global variable
-      let qiskitScript =
-        "\nqc = ccx(" +
-        document.getElementById("control-qubit-1").value +
-        ", " +
-        document.getElementById("control-qubit-2").value +
-        ", " +
-        document.getElementById("target-qubit").value +
-        ")";
+    node.on("input", function (msg, send, done) {
+      let qrConfig = msg.payload;
 
-      let oldScript = globalContext.get("script");
-      globalContext.set("script", oldScript + qiskitScript);
-
-      // Creating an array of messages to be sent
-      // Each message represents a different qubit
-      for (let i = 0; i < node.outputs; i++) {
-        output[i] = {
-          topic: "Quantum Circuit",
-          payload: {
-            register: "qr" + msg.payload.register.toString(),
-            qubit: i,
-          },
-        };
+      if (qrConfig.length == 3) {
       }
 
-      // Sending one qubit object per node output
       send(output);
     });
   }
