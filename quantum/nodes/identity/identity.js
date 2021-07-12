3a6eda8f709f23b13281bf4d3875b4c328415635
Theo Reignier <ucabteb@ucl.ac.uk>
2021-07-12 18:35:00 +0200

    Creation of an identity gate node (does not do anything, just for flow structure purposes)

---
+++ quantum/nodes/identity/identity.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = function(RED) {
+  function IdentityNode(config) {
+    // Creating node with properties
+    RED.nodes.createNode(this, config);
+    this.name = config.name;
+
+    this.on('input', async function(msg, send, done) {
+      // Throw a connection error if:
+      // - The user connects it to a node that is not from the quantum library
+      // - The user does not input a qubit object in the node
+      // - The user chooses to use registers but does not initiate them
+      if (msg.topic !== 'Quantum Circuit') {
+        throw new Error(
+            'Identity gates must be connected to nodes from the quantum library only.',
+        );
+      } else if (typeof(msg.payload.register) === 'undefined' && typeof(msg.payload.qubit) === 'undefined') {
+        throw new Error(
+            'Identity gates must receive qubits objects as inputs.\n' +
+            'Please use "Quantum Circuit" & "Quantum Register" nodes to generate qubits objects.',
+        );
+      } else if (typeof(msg.payload.qubit) === 'undefined') {
+        throw new Error(
+            'If "Registers & Bits" was selected in the "Quantum Circuit" node, please make use of register nodes.',
+        );
+      }
+
+      send(msg);
+    });
+  }
+
+  RED.nodes.registerType('identity', IdentityNode);
+};
