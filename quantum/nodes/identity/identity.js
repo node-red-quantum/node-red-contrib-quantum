4f777ba722d29a788d70e96b57c4d7f02afe40b7
Theo Reignier <ucabteb@ucl.ac.uk>
2021-07-13 12:02:28 +0200

    Using node status to identify qubits using the identity node

--- quantum/nodes/identity/identity.js
+++ quantum/nodes/identity/identity.js
@@ -5,8 +5,9 @@ module.exports = function(RED) {
     // Creating node with properties
     RED.nodes.createNode(this, config);
     this.name = config.name;
+    const node = this;
 
-    this.on('input', async function(msg, send, done) {
+    this.on('input', function(msg, send, done) {
       // Throw a connection error if:
       // - The user connects it to a node that is not from the quantum library
       // - The user does not input a qubit object in the node
@@ -24,6 +25,21 @@ module.exports = function(RED) {
         throw new Error(
             'If "Registers & Bits" was selected in the "Quantum Circuit" node, please make use of register nodes.',
         );
+      }
+
+      // Using node status to inform the user of which qubit is being transmitted
+      if (typeof(msg.payload.register) === 'undefined') {
+        node.status({
+          fill: 'grey',
+          shape: 'dot',
+          text: 'Qubit ' + msg.payload.qubit.toString(),
+        });
+      } else {
+        node.status({
+          fill: 'grey',
+          shape: 'dot',
+          text: 'Register ' + msg.payload.register + ' / Qubit ' + msg.payload.qubit.toString(),
+        });
       }
 
       // Simply return the msg received without any operations
