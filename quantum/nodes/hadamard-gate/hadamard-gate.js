c40ccacf3f9b909582c06cc5e1bc8c121eb645c9
bbaayezi <bbaayezi@gmail.com>
2021-07-14 18:56:30 +0100

    modify hadamard gate to execute python shell

--- quantum/nodes/hadamard-gate/hadamard-gate.js
+++ quantum/nodes/hadamard-gate/hadamard-gate.js
@@ -1,20 +1,33 @@
-module.exports = function(RED) {
-  'use strict';
+'use strict';
 
+const util = require('util');
+const snippets = require('../../snippets');
+const shell = require('../../python').PythonShell;
+
+module.exports = function(RED) {
   function HadamardGate(config) {
     RED.nodes.createNode(this, config);
-    const globalContext = this.context().global;
     const node = this;
 
-    node.on('input', (msg, send, done) => {
+    node.on('input', async (msg, send, done) => {
       let qrConfig = msg.payload;
       let keys = Object.keys(qrConfig);
       if (!keys.includes('register') || !keys.includes('qubit')) {
         throw new Error('Invalid Input');
       }
-      let newScript = `\nqc.h(${qrConfig.register}[${qrConfig.qubit}])`;
-      let oldScript = globalContext.get('script');
-      globalContext.set('script', oldScript + newScript);
+      // show the status of the node
+      node.status({
+        fill: 'grey',
+        shape: 'dot',
+        text: qrConfig.register ? `Register ${qrConfig.register} / Qubit ${qrConfig.qubit}` :
+          `Qubit ${qrConfig.qubit}`,
+      });
+      let shellScript = util.format(snippets.HADAMARD_GATE, qrConfig.register ? 
+        `${qrConfig.register}[${qrConfig.qubit}]` :
+        `${qrConfig.qubit}`);
+      await shell.execute(shellScript, (err) => {
+        if (err) node.error(err);
+      });
       // pass the quantum register config to the output
       send(msg);
     })
