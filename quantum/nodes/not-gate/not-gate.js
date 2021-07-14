15ec624b1bd8ca1bd85d82957b5cb212023aeccb
Wiruson <chonlam88@gmail.com>
2021-07-15 01:15:34 +0800

    Update quantum/nodes/not-gate/not-gate.js
    Co-authored-by: Louis Lefevre <louislefev@gmail.com>

--- quantum/nodes/not-gate/not-gate.js
+++ quantum/nodes/not-gate/not-gate.js
@@ -7,47 +7,31 @@ module.exports = function(RED) {
   }
 
   function NotGate(config){
-
     RED.nodes.createNode(this, config);
-
     this.name = config.name;
     this.outputs = config.outputs;
-
     const globalContext = this.context().global;
     const node = this;
     const output = new Array(node.outputs);
 
-    //creating not gate here.
-
     node.on("input", (msg, send, done) => {
-
       let qiskitScript = "\nqc = x("+ document.getElementByID("control-qubit-1").value +")";
       let oldScript = globalContext.get("script");
       globalContext.set("script", oldScript + qiskitScript);
 
       for (let i = 0; i < node.outputs; i++){
-
         output[i] = {
-
           topic: "Quantum Circuit",
           payload: {
-
             register: "qr" + msg.payload.register.toString(),
             qubit: i,
-
           },
-
         };
-
       }
 
       send(output);
-
     })
-    
-
   }
-
 
   RED.nodes.registerType('not-gate', NodeTemplateNode);
 };
