f16f9f488ef254816528afbad5013b15e1cdb65b
Louis Lefevre <louislefev@gmail.com>
2021-07-16 00:46:47 +0100

    Fix commands to work on Windows
    Python command and clean script fixes for running on Windows.

--- bin/pyvenv.sh
+++ bin/pyvenv.sh
@@ -4,17 +4,28 @@
 # Note that this is designed to be run in POSIX-compatible environments
 # which use Bash and has not been fully tested.
 
+# Check OS for paths.
+venv="$PWD/venv"
+if [[ "$OSTYPE" == "msys" ]] || [[ "$OSTYPE" == "cygwin" ]]; then
+  python="python"
+  python_path="$venv/Scripts/python.exe"
+  pip_path="$venv/Scripts/pip.exe"
+else
+  python="python3"
+  python_path="$venv/bin/python"
+  pip_path="$venv/bin/pip"
+fi
+
 # Check if python is installed. If no, exit unsuccessfully.
-if ! command -v python3 &>/dev/null; then
+if ! command -v "$python" &>/dev/null; then
   echo "Error: failed to find Python 3 in PATH"
   exit 1
 fi
 
 # Check if virtual environment exists. If no, create it.
-venv="$PWD/venv"
 if [[ ! -d "$venv" ]] || [[ -z "$venv" ]]; then
   echo "Creating virtual environment at $venv..."
-  if python3 -m venv "$venv"; then
+  if "$python" -m venv "$venv"; then
     echo "Successfully created virtual environment"
   else
     echo "Error: failed to create virtual environment"
@@ -24,36 +35,27 @@ else
   echo "Using virtual environment at $venv"
 fi
 
-# Check OS for paths.
-if [[ "$OSTYPE" == "mysys" ]] || [[ "$OSTYPE" == "cygwin" ]]; then
-  python="$venv/Scripts/python.exe"
-  pip="$venv/Scripts/pip.exe"
-else
-  python="$venv/bin/python"
-  pip="$venv/bin/pip"
-fi
-
 # Check if Qiskit package is installed. If yes, exit successfully.
-if [[ -x "$python" ]]; then
-  if "$python" -c "import qiskit" &>/dev/null; then
+if [[ -x "$python_path" ]]; then
+  if "$python_path" -c "import qiskit" &>/dev/null; then
     echo "Qiskit is installed"
     exit 0
   fi
 else
-  echo "Error: failed to execute $python"
+  echo "Error: failed to execute $python_path"
   exit 1
 fi
 
 # Install Qiskit within the virtual environment using pip.
-if [[ -x "$pip" ]]; then
+if [[ -x "$pip_path" ]]; then
   echo "Installing Qiskit..."
-  if "$pip" install qiskit; then
+  if "$pip_path" install qiskit; then
     echo "Successfully installed Qiskit"
   else
     echo "Error: failed to install Qiskit"
     exit 1
   fi
 else
-  echo "Error: failed to execute $pip"
+  echo "Error: failed to execute $pip_path"
   exit 1
 fi
