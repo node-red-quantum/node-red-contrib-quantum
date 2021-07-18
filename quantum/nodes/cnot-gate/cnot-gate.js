4f2821b9bdf5ceadd732a806d5fffcb02824bf84
zhiqiang <happysky@happyskydeMacBook-Pro.local>
2021-07-18 09:02:49 +0100

    The qubits received as input are msg objects with a payload property

--- quantum/nodes/cnot-gate/cnot-gate.js
+++ quantum/nodes/cnot-gate/cnot-gate.js
@@ -1,5 +1,4 @@
 'use strict';
-
 const util = require('util');
 const snippets = require('../../snippets');
 const shell = require('../../python').PythonShell;
@@ -12,28 +11,26 @@ module.exports = function(RED) {
     this.targetPosition = config.targetPosition;
     const node = this;
 
-    node.on('input', async function(msg, send, done) {
+    this.on('input', async function(msg, send, done) {
       // Throw a connection error if:
-      // - The user connects it to a node that is not from the quantum library
-      // - The user does not input a qubit object in the node
-      // - The user chooses to use registers but does not initiate them
-      // - The user does not input 2 qubits.
+      // - The user connects it to a node that is not from the quantum library.
+      // - The user does not input a qubit object in the node.
+      // - The user chooses to use registers but does not initiate them.
       if (msg.topic !== 'Quantum Circuit') {
         throw new Error(
             'The CNot Gate must be connected to nodes from the quantum library only.',
         );
-      } else if (typeof(msg.payload.register) === 'undefined' && typeof(msg.payload.qubit) === 'undefined') {
+      } else if (
+        typeof msg.payload.register === 'undefined' &&
+        typeof msg.payload.qubit === 'undefined'
+      ) {
         throw new Error(
             'The CNot Gate nodes must receive qubits objects as inputs.\n' +
             'Please use "Quantum Circuit" & "Quantum Register" nodes to generate qubits objects.',
         );
-      } else if (typeof(msg.payload.qubit) === 'undefined') {
+      } else if (typeof msg.payload.qubit === 'undefined') {
         throw new Error(
             'If "Registers & Bits" was selected in the "Quantum Circuit" node, please make use of register nodes.',
-        );
-      } else if (node.qubits.length !== 2) {
-        throw new Error(
-            'The CNot Gate requires exactly 2 input qubits to be used.',
         );
       }
 
@@ -41,9 +38,9 @@ module.exports = function(RED) {
       node.qubits.push(msg);
 
       // If all qubits have arrived, we first reorder the node.qubits array for output consistency
-      if (node.qubits.length === 2) {
+      if (node.qubits.length == 2) {
         node.qubits.sort(function compare(a, b) {
-          if (typeof (a.payload.register) !== 'undefined') {
+          if (typeof a.payload.register !== 'undefined') {
             const regA = parseInt(a.payload.registerVar.slice(2));
             const regB = parseInt(b.payload.registerVar.slice(2));
             if (regA < regB) return -1;
@@ -58,28 +55,40 @@ module.exports = function(RED) {
           }
         });
 
-        if (targetPosition === 'upper') {
-          let targetQubit = node.qubits[0];
-          let controlQubit = node.qubits[1];
+        // Initialise qubit variables for script.
+        let controlQubit = node.qubits[0];
+        let targetQubit = node.qubits[0];
+
+        // Determine which node is the target based on position.
+        if (node.targetPosition == 'Upper') {
+          targetQubit = node.qubits[0];
+          controlQubit = node.qubits[1];
         } else {
-          let targetQubit = node.qubits[1];
-          let controlQubit = node.qubits[0];
+          targetQubit = node.qubits[1];
+          controlQubit = node.qubits[0];
         }
 
-        // Generate the corresponding cnot-gate Qiskit script
-        if (typeof msg.payload.register === 'undefined') {
-          let cnotScript = util.format(
-              snippets.CNOT_GATE,
-              controlQubit.qubit.toString(),
-              targetQubit.qubit.toString(),
-          );
-        } else {
-          let cnotScript = util.format(
-              snippets.CNOT_GATE,
-              controlQubit.registerVar + '[' + controlQubit.qubit.toString() + ']',
-              targetQubit.registerVar + '[' + targetQubit.qubit.toString() + ']',
-          );
-        }
+        // Generate the corresponding CNot Gate Qiskit script
+        let cnotScript = '';
+        node.qubits.map((msg) => {
+          // Use qubits only if there are no registers.
+          if (typeof msg.payload.register === 'undefined') {
+            cnotScript = util.format(
+                snippets.CNOT_GATE,
+                controlQubit.payload.qubit.toString(),
+                targetQubit.payload.qubit.toString(),
+            );
+          } else {
+            // Use registers if there are quantum registers.
+            cnotScript = util.format(
+                snippets.CNOT_GATE,
+                controlQubit.payload.registerVar + '[' +
+                controlQubit.payload.qubit.toString() + ']',
+                targetQubit.payload.registerVar + '[' +
+                targetQubit.payload.qubit.toString() + ']',
+            );
+          }
+        });
 
         // Run the script in the python shell
         await shell.execute(cnotScript, (err) => {
