d02c7c6c1ec3889ffc32db4bf07f20938f04a82b
Khesim Reid <khesim.reid@gmail.com>
2021-07-27 16:29:05 +0100

    Reset Merges and Fix package.json node filepath

--- quantum/nodes/ibm-quantum-system/ibm-quantum-system.js
+++ quantum/nodes/ibm-quantum-system/ibm-quantum-system.js
@@ -6,20 +6,20 @@ module.exports = function (RED) {
     const node = this;
     const api_key = config.api_key;
 
-    //Script for is user decided to use default provider and backend for their account.
-    let ibmqScript = `provider = IBMQ.enable_account(${api_key})
-    backend_service = provider.service('backend')
-    job = execute(qc, backend=backend_service)
-    results = job.result()`;
+    // //Script for is user decided to use default provider and backend for their account.
+    // let ibmqScript = `provider = IBMQ.enable_account(${api_key})
+    // backend_service = provider.service('backend')
+    // job = execute(qc, backend=backend_service)
+    // results = job.result()`;
 
-    await shell.execute(ibmqScript, (err, data) => {
-      if (err) {
-        node.error(err, msg);
-      } else {
-        msg.payload = data;
-        send(msg);
-      }
-    });
+    // await shell.execute(ibmqScript, (err, data) => {
+    //   if (err) {
+    //     node.error(err, msg);
+    //   } else {
+    //     msg.payload = data;
+    //     send(msg);
+    //   }
+    // });
   }
   RED.nodes.registerType("ibm-quantum-system", IBMQuantumSystemNode);
 };
