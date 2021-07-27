ecda2900aa462a68ff250038e2cd94cd9efbf085
Khesim Reid <khesim.reid@gmail.com>
2021-07-27 18:25:43 +0100

    Add input field for desired user quantum system.

--- quantum/nodes/ibm-quantum-system/ibm-quantum-system.js
+++ quantum/nodes/ibm-quantum-system/ibm-quantum-system.js
@@ -5,21 +5,37 @@ module.exports = function (RED) {
     RED.nodes.createNode(this, config);
     const node = this;
     const api_key = config.api_key;
+    const preferred_provider = config.preferred_provider;
+    const preferred_backend = config.preferred_backend;
 
-    // //Script for is user decided to use default provider and backend for their account.
-    // let ibmqScript = `provider = IBMQ.enable_account(${api_key})
-    // backend_service = provider.service('backend')
-    // job = execute(qc, backend=backend_service)
-    // results = job.result()`;
+    let ibmqScript = "";
 
-    // await shell.execute(ibmqScript, (err, data) => {
-    //   if (err) {
-    //     node.error(err, msg);
-    //   } else {
-    //     msg.payload = data;
-    //     send(msg);
-    //   }
-    // });
+    //Scripts dependent on if user wants default machines for their account or to specify another set of parameters.
+    if (preferred_provider == "" && preferred_backend == "") {
+      ibmqScript = `provider = IBMQ.enable_account(${api_key})
+      backend_service = provider.service('backend')
+      job = execute(qc, backend=backend_service)
+      results = job.result()`;
+    } else if (preferred_provider != "" && preferred_backend == "") {
+      ibmqScript = `provider = IBMQ.get_provider(${preferred_provider})
+      backend_service = IBMQ.least_busy(provider.backends())
+      job = execute(qc, backend=backend_service)
+      results = job.result()`;
+    } else {
+      ibmqScript = `provider = IBMQ.get_provider(${preferred_provider})
+      backend_service = IBMQ.get_backend(${preferred_backend})
+      job = execute(qc, backend=backend_service)
+      results = job.result()`;
+    }
+
+    await shell.execute(ibmqScript, (err, data) => {
+      if (err) {
+        node.error(err, msg);
+      } else {
+        msg.payload = data;
+        send(msg);
+      }
+    });
   }
   RED.nodes.registerType("ibm-quantum-system", IBMQuantumSystemNode);
 };
