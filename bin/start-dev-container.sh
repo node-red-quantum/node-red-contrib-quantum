db9bc750001bf7c7532e67062e8dbd1ffe54020f
bbaayezi <bbaayezi@gmail.com>
2021-07-30 20:24:27 +0100

    enable hot reload

--- bin/start-dev-container.sh
+++ bin/start-dev-container.sh
@@ -19,16 +19,22 @@ else
     echo "Development container not running"
     if docker image ls | grep -q $DEV_IMAGE_NAME; then
         echo "Image found, starting dev container"
-        docker run -it --user=root -p 1880:1880 --name $DEV_CONTAINER_NAME \
+        docker run -itd --user=root -p 1880:1880 --name $DEV_CONTAINER_NAME \
         -v node_modules:/node-red-contrib-quantum/node_modules \
         -v venv:/node-red-contrib-quantum/venv \
-        -v ~/.node-red/dev-container:/root/.node-red $DEV_IMAGE_NAME
+        -v ${PWD}:/node-red-contrib-quantum \
+        -v ~/.node-red/dev-container:/root/.node-red $DEV_IMAGE_NAME && \
+        docker exec -it $DEV_CONTAINER_NAME bash -c \
+        "cd /root/.node-red && npm install /node-red-contrib-quantum && cd /node-red-contrib-quantum && nodemon --exec node-red flows_quantum.json"
     else
         echo "Image not found, building image and starting dev container"
         docker build -f Dockerfile.dev . -t $DEV_IMAGE_NAME && \
-        docker run -it --user=root -p 1880:1880 --name $DEV_CONTAINER_NAME \
+        docker run -itd --user=root -p 1880:1880 --name $DEV_CONTAINER_NAME \
         -v node_modules:/node-red-contrib-quantum/node_modules \
         -v venv:/node-red-contrib-quantum/venv \
-        -v ~/.node-red/dev-container:/root/.node-red $DEV_IMAGE_NAME
+        -v ${PWD}:/node-red-contrib-quantum \
+        -v ~/.node-red/dev-container:/root/.node-red $DEV_IMAGE_NAME && \
+        docker exec -it $DEV_CONTAINER_NAME bash -c \
+        "cd /root/.node-red && npm install /node-red-contrib-quantum && cd /node-red-contrib-quantum && nodemon --exec node-red flows_quantum.json"
     fi
 fi
