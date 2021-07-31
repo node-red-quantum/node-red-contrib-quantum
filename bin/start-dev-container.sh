27b84ef044654abbacb36df2e5663f483049e14d
bbaayezi <bbaayezi@gmail.com>
2021-07-31 13:17:04 +0100

    install dependencies in container

--- bin/start-dev-container.sh
+++ bin/start-dev-container.sh
@@ -1,7 +1,9 @@
 #!/bin/bash 
 
 DEV_IMAGE_NAME="docker-quantum"
-DEV_CONTAINER_NAME="quantum_dev"
+DEV_CONTAINER_NAME="quantum-dev"
+DEV_DEPENDENCY_NODE="quantum-dep-node"
+DEV_DEPENDENCY_PYTHON="quantum-dep-venv"
 
 # check if docker is installed
 if ! command -v docker &> /dev/null
@@ -18,11 +20,19 @@ if docker container ls --all | grep -q $DEV_CONTAINER_NAME; then
     "nodemon --exec node-red flows_quantum.json"
 else
     echo "Development container not running"
+    if ! docker volume ls | grep -q $DEV_DEPENDENCY_NODE; then
+        echo "Creating volumes to store node dev dependencies"
+        docker volume create $DEV_DEPENDENCY_NODE
+    fi
+    if ! docker volume ls | grep -q $DEV_DEPENDENCY_PYTHON; then
+        echo "Creating volumes to store python dev dependencies"
+        docker volume create $DEV_DEPENDENCY_PYTHON
+    fi
     if docker image ls | grep -q $DEV_IMAGE_NAME; then
         echo "Image found, starting dev container"
         docker run -itd --user=root -p 1880:1880 --name $DEV_CONTAINER_NAME \
-        -v node_modules:/node-red-contrib-quantum/node_modules \
-        -v venv:/node-red-contrib-quantum/venv \
+        -v $DEV_DEPENDENCY_NODE:/node-red-contrib-quantum/node_modules \
+        -v $DEV_DEPENDENCY_PYTHON:/node-red-contrib-quantum/venv \
         -v ${PWD}:/node-red-contrib-quantum \
         -v ~/.node-red/dev-container:/root/.node-red $DEV_IMAGE_NAME && \
         docker exec -it $DEV_CONTAINER_NAME bash -c \
@@ -31,8 +41,8 @@ else
         echo "Image not found, building image and starting dev container"
         docker build -f Dockerfile.dev . -t $DEV_IMAGE_NAME && \
         docker run -itd --user=root -p 1880:1880 --name $DEV_CONTAINER_NAME \
-        -v node_modules:/node-red-contrib-quantum/node_modules \
-        -v venv:/node-red-contrib-quantum/venv \
+        -v $DEV_DEPENDENCY_NODE:/node-red-contrib-quantum/node_modules \
+        -v $DEV_DEPENDENCY_PYTHON:/node-red-contrib-quantum/venv \
         -v ${PWD}:/node-red-contrib-quantum \
         -v ~/.node-red/dev-container:/root/.node-red $DEV_IMAGE_NAME && \
         docker exec -it $DEV_CONTAINER_NAME bash -c \
