7f4917392f67105ee0b9128fbb76420240283ce7
Khesim Reid <khesim.reid@gmail.com>
2021-08-05 15:37:33 +0100

    Fixed formatting.

--- test/quantum/toffoli-gate_spec.js
+++ test/quantum/toffoli-gate_spec.js
@@ -1,49 +1,49 @@
-var helper = require("node-red-node-test-helper");
-var toffoliGateNode = require("../quantum/toffoli-gate.js");
+let helper = require('node-red-node-test-helper');
+let toffoliGateNode = require('../quantum/toffoli-gate.js');
 
-describe("toffoli-gate Node", function () {
-  before(function (done) {
+describe('toffoli-gate Node', function() {
+  before(function(done) {
     helper.startServer(done);
   });
 
-  afterEach(function () {
+  afterEach(function() {
     helper.unload();
   });
 
-  after(function (done) {
+  after(function(done) {
     helper.stopServer(done);
   });
 
-  it("should be loaded", function (done) {
-    var flow = [
+  it('should be loaded', function(done) {
+    let flow = [
       {
-        id: "n1",
-        type: "toffoli-gate",
-        name: "test name",
-        targetPosition: "Top",
+        id: 'n1',
+        type: 'toffoli-gate',
+        name: 'test name',
+        targetPosition: 'Top',
       },
     ];
-    helper.load(toffoliGateNode, flow, function () {
-      var n1 = helper.getNode("n1");
-      n1.should.have.property("name", "targetPosition");
+    helper.load(toffoliGateNode, flow, function() {
+      let n1 = helper.getNode('n1');
+      n1.should.have.property('name', 'targetPosition');
       done();
     });
   });
 
-  it("should apply toffoli gate to 3 input qubits", function (done) {
-    var flow = [
-      { id: "n3", type: "quantum-circuit", wires: [["n2"]] },
-      { id: "n2", type: "toffoli-gate", wires: [["n3"]] },
-      { id: "n3", type: "helper" },
+  it('should apply toffoli gate to 3 input qubits', function(done) {
+    let flow = [
+      {id: 'n3', type: 'quantum-circuit', wires: [['n2']]},
+      {id: 'n2', type: 'toffoli-gate', wires: [['n3']]},
+      {id: 'n3', type: 'helper'},
     ];
-    helper.load(toffoliGateNode, flow, function () {
-      var n2 = helper.getNode("n2");
-      var n1 = helper.getNode("n1");
-      n2.on("input", function (msg) {
-        msg.should.have.property("payload", "uppercase");
+    helper.load(toffoliGateNode, flow, function() {
+      let n2 = helper.getNode('n2');
+      let n1 = helper.getNode('n1');
+      n2.on('input', function(msg) {
+        msg.should.have.property('payload', 'uppercase');
         done();
       });
-      n1.receive({ payload: "UpperCase" });
+      n1.receive({payload: 'UpperCase'});
     });
   });
 });
