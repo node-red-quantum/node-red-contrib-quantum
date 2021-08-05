68bd3137f9675ba3228450372467b6269efed9b2
Theo Reignier <ucabteb@ucl.ac.uk>
2021-08-05 17:37:09 +0200

    Improved qubit waiting phase logic and fixed a bug

--- quantum/nodes/ibm-quantum-system/ibm-quantum-system.js
+++ quantum/nodes/ibm-quantum-system/ibm-quantum-system.js
@@ -35,7 +35,7 @@ module.exports = function(RED) {
       }
 
       // If the quantum circuit does not have registers
-      if (typeof msg.payload.register === 'undefined') {
+      if (typeof(msg.payload.register) === 'undefined') {
         node.qreg = undefined;
         node.qubits.push(msg);
 
@@ -48,16 +48,18 @@ module.exports = function(RED) {
         // Keep track of qubits that have arrived and the remaining ones
         if (node.qubits.length == 0) node.qreg = {};
 
-        // Throw an error if too many qubits are received by the quantum system node
+        // Throw an error if too many qubits are received by the simulator node
         // because the user connected qubits from different quantum circuits
-        if (
-          (!Object.keys(node.qreg).includes(msg.payload.registerVar) &&
-            Object.keys(node.qreg).length == msg.payload.structure.qreg) ||
-          (Object.keys(node.qreg).includes(msg.payload.registerVar) &&
-            node.qreg[msg.payload.registerVar].count ==
-              node.qreg[msg.payload.registerVar].total)
-        ) {
+        if ((
+          !Object.keys(node.qreg).includes(msg.payload.registerVar) &&
+          Object.keys(node.qreg).length == msg.payload.structure.qreg
+        ) || (
+          Object.keys(node.qreg).includes(msg.payload.registerVar) &&
+          node.qreg[msg.payload.registerVar].count == node.qreg[msg.payload.registerVar].total
+        )) {
           done(new Error(errors.QUBITS_FROM_DIFFERENT_CIRCUITS));
+          reset();
+          return;
         }
 
         // Storing information about which qubits were received
@@ -73,11 +75,15 @@ module.exports = function(RED) {
         node.qubits.push(msg);
 
         // Checking whether all qubits have arrived or not
-        Object.keys(node.qreg).map((key) => {
-          if (node.qreg[key].count < node.qreg[key].total) {
-            qubitsArrived = false;
-          }
-        });
+        if (Object.keys(node.qreg).length == msg.payload.structure.qreg) {
+          Object.keys(node.qreg).map((key) => {
+            if (node.qreg[key].count < node.qreg[key].total) {
+              qubitsArrived = false;
+            }
+          });
+        } else {
+          qubitsArrived = false;
+        }
       }
 
       // If all qubits have arrived,
