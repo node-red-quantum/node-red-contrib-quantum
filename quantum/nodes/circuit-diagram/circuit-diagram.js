bd6f855a08c31197fac011b08051c030acb270ac
Theo Reignier <ucabteb@ucl.ac.uk>
2021-08-05 17:16:10 +0200

    Fixed qubit waiting phase for homogeneity and incorrect logic

--- quantum/nodes/circuit-diagram/circuit-diagram.js
+++ quantum/nodes/circuit-diagram/circuit-diagram.js
@@ -35,18 +35,20 @@ module.exports = function(RED) {
       } else {
         // If the quantum circuit has registers
         // Keep track of qubits that have arrived and the remaining ones
-        if (node.qubits.length === 0) node.qreg = {};
+        if (node.qubits.length == 0) node.qreg = {};
 
-        // Throw an error if too many qubits are received by this node
+        // Throw an error if too many qubits are received by the simulator node
         // because the user connected qubits from different quantum circuits
         if ((
           !Object.keys(node.qreg).includes(msg.payload.registerVar) &&
-          Object.keys(node.qreg).length === msg.payload.structure.qreg
+          Object.keys(node.qreg).length == msg.payload.structure.qreg
         ) || (
           Object.keys(node.qreg).includes(msg.payload.registerVar) &&
-          node.qreg[msg.payload.registerVar].count === node.qreg[msg.payload.registerVar].total
+          node.qreg[msg.payload.registerVar].count == node.qreg[msg.payload.registerVar].total
         )) {
           done(new Error(errors.QUBITS_FROM_DIFFERENT_CIRCUITS));
+          reset();
+          return;
         }
 
         // Storing information about which qubits were received
@@ -61,33 +63,16 @@ module.exports = function(RED) {
 
         node.qubits.push(msg);
 
-        // If this node has inputs from more q-registers than there are in the circuit
-        if (Object.keys(node.qreg).length > msg.payload.structure.qreg) {
-          throw new Error(
-              // eslint-disable-next-line max-len
-              'Only qubits of quantum registers from the same quantum circuit should be connected to the circuit-diagram node.',
-          );
-        } else if (Object.keys(node.qreg).length === msg.payload.structure.qreg) {
+        // Checking whether all qubits have arrived or not
+        if (Object.keys(node.qreg).length == msg.payload.structure.qreg) {
           Object.keys(node.qreg).map((key) => {
-            // If this node has inputs from more qubits than there are in a register
-            if (node.qreg[key].count > node.qreg[key].total) {
-              throw new Error(
-                  'Only qubits from a single quantum circuit should be connected to the circuit-diagram node.',
-              );
-              // If not all qubits have arrived
-            } else if (node.qreg[key].count < node.qreg[key].total) {
+            if (node.qreg[key].count < node.qreg[key].total) {
               qubitsArrived = false;
             }
           });
         } else {
           qubitsArrived = false;
         }
-        // Checking whether all qubits have arrived or not
-        Object.keys(node.qreg).map((key) => {
-          if (node.qreg[key].count < node.qreg[key].total) {
-            qubitsArrived = false;
-          }
-        });
       }
 
       // If all qubits have arrived,
