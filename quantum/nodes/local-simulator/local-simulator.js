82999ad836a16eac779994d837cc7ca09563c95e
Theo Reignier <ucabteb@ucl.ac.uk>
2021-08-05 17:18:12 +0200

    Fixed qubits waiting phase logic error and consistency

--- quantum/nodes/local-simulator/local-simulator.js
+++ quantum/nodes/local-simulator/local-simulator.js
@@ -73,33 +73,16 @@ module.exports = function(RED) {
 
         node.qubits.push(msg);
 
-        // If the simulator node has inputs from more q-registers than there are in the circuit
-        if (Object.keys(node.qreg).length > msg.payload.structure.qreg) {
-          throw new Error(
-              // eslint-disable-next-line max-len
-              'Only qubits of quantum registers from the same quantum circuit should be connected to the simulator node.',
-          );
-        } else if (Object.keys(node.qreg).length == msg.payload.structure.qreg) {
+        // Checking whether all qubits have arrived or not
+        if (Object.keys(node.qreg).length == msg.payload.structure.qreg) {
           Object.keys(node.qreg).map((key) => {
-            // If the simulator node has inputs from more qubits than there are in a register
-            if (node.qreg[key].count > node.qreg[key].total) {
-              throw new Error(
-                  'Only qubits from a single quantum circuit should be connected to the simulator node.',
-              );
-              // If not all qubits have arrived
-            } else if (node.qreg[key].count < node.qreg[key].total) {
+            if (node.qreg[key].count < node.qreg[key].total) {
               qubitsArrived = false;
             }
           });
         } else {
           qubitsArrived = false;
         }
-        // Checking whether all qubits have arrived or not
-        Object.keys(node.qreg).map((key) => {
-          if (node.qreg[key].count < node.qreg[key].total) {
-            qubitsArrived = false;
-          }
-        });
       }
 
       // If all qubits have arrived,
