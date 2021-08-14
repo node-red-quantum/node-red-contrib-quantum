18c89ed2c37eceddfc9a2e4d5b324655c3e09c61
Louis Lefevre <louislefev@gmail.com>
2021-08-14 04:53:40 +0100

    Add flow builder example

--- test/nodes/local-simulator_spec.js
+++ test/nodes/local-simulator_spec.js
@@ -1,6 +1,9 @@
 const localSimulatorNode = require('../../quantum/nodes/local-simulator/local-simulator.js');
 const testUtil = require('../test-util');
+const shell = require('../../quantum/python.js').PythonShell;
 const nodeTestHelper = testUtil.nodeTestHelper;
+const {FlowBuilder} = require('../flow-builder');
+const assert = require('chai').assert;
 
 
 describe('LocalSimulatorNode', function() {
@@ -9,11 +12,37 @@ describe('LocalSimulatorNode', function() {
   });
 
   afterEach(function(done) {
+    shell.stop();
     nodeTestHelper.unload();
     nodeTestHelper.stopServer(done);
   });
 
   it('load node', function(done) {
     testUtil.isLoaded(localSimulatorNode, 'local-simulator', done);
+  });
+
+  it('execute script', function(done) {
+    const flow = new FlowBuilder();
+    flow.addQuantumCircuit('n0', ['n1'], {structure: 'qubits', outputs: '1', qbitsreg: '1', cbitsreg: '1'});
+    flow.addHadamardGate('n1', ['n2']);
+    flow.addMeasure('n2', ['n3'], {selectedBit: '0'});
+    flow.addLocalSimulator('n3', ['n4'], {shots: '1'});
+    flow.addHelper('n4', []);
+
+    nodeTestHelper.load(flow.nodes, flow.flow, function() {
+      let n0 = nodeTestHelper.getNode('n0');
+      let n4 = nodeTestHelper.getNode('n4');
+
+      n4.on('input', function(msg) {
+        try {
+          assert.isNotEmpty(msg.payload);
+          done();
+        } catch (err) {
+          done(err);
+        }
+      });
+
+      n0.receive({payload: ''});
+    });
   });
 });
