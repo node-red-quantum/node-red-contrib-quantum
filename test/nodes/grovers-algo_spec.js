e966b23e2e273d8714bd6b9e1a2e1413c0ff5da3
Timur Toktassynov <timur.toktassynov@gmail.com>
2021-08-14 12:43:07 +0100

    added two test cases

--- test/nodes/grovers-algo_spec.js
+++ test/nodes/grovers-algo_spec.js
@@ -1,19 +1,54 @@
 const groversAlgoNode = require('../../quantum/nodes/grovers-algo/grovers-algo.js');
 const testUtil = require('../test-util');
 const nodeTestHelper = testUtil.nodeTestHelper;
+const assert = require('chai').assert;
+const shell = require('../../quantum/python').PythonShell;
 
 
 describe('GroversAlgoNode', function() {
+  this.timeout(10000);
   beforeEach(function(done) {
     nodeTestHelper.startServer(done);
   });
 
   afterEach(function(done) {
+    shell.stop();
     nodeTestHelper.unload();
     nodeTestHelper.stopServer(done);
   });
 
   it('load node', function(done) {
     testUtil.isLoaded(groversAlgoNode, 'grovers-algo', done);
+  });
+
+  it('default name outputs correctly', function(done) {
+    var flow = [{ id: "groversNode", type: "grovers-algo", wires: []}]
+    nodeTestHelper.load(groversAlgoNode, flow, function () {
+      var groversNode = nodeTestHelper.getNode("groversNode");
+      groversNode.should.have.property('name', "Grover's algorithm");
+      done();
+    });
+  });
+
+  it('return success output on valid input', function(done) {
+    var flow = [{ id: "groversNode", type: "grovers-algo", wires: [["helperNode"]]},
+      { id: "helperNode", type: "helper" }]
+    nodeTestHelper.load(groversAlgoNode, flow, function () {
+      var groversNode = nodeTestHelper.getNode("groversNode");
+      var helperNode = nodeTestHelper.getNode("helperNode");
+      helperNode.on("input", function (msg) {
+        const expectedPayload = 'Success!\n' +
+          'Top measurement: 111111\n' +
+          'iterations = 6';
+        try {
+          msg.should.have.property('payload', expectedPayload);
+          done();
+        }
+        catch(err){
+          done(err);
+        }
+      });
+      groversNode.receive({ payload: "111111" });
+    });
   });
 });
