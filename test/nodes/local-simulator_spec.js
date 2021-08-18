6ff8eee49309a48dd2cf733bac12c2cb4b5bf006
Louis Lefevre <louislefev@gmail.com>
2021-08-18 18:20:53 +0100

    Add execute script tests for measure and simulator

--- test/nodes/local-simulator_spec.js
+++ test/nodes/local-simulator_spec.js
@@ -1,10 +1,8 @@
 const util = require('util');
-const assert = require('chai').assert;
 const testUtil = require('../test-util');
 const nodeTestHelper = testUtil.nodeTestHelper;
 const {FlowBuilder} = require('../flow-builder');
 const localSimulatorNode = require('../../quantum/nodes/local-simulator/local-simulator.js');
-const shell = require('../../quantum/python.js').PythonShell;
 const snippets = require('../../quantum/snippets.js');
 
 
@@ -14,7 +12,6 @@ describe('LocalSimulatorNode', function() {
   });
 
   afterEach(function(done) {
-    shell.stop();
     nodeTestHelper.unload();
     nodeTestHelper.stopServer(done);
   });
@@ -23,29 +20,15 @@ describe('LocalSimulatorNode', function() {
     testUtil.isLoaded(localSimulatorNode, 'local-simulator', done);
   });
 
-  it('execute script', function(done) {
-    const flow = new FlowBuilder();
+  it('execute command', function(done) {
+    let command = util.format(snippets.LOCAL_SIMULATOR, '1');
+    let flow = new FlowBuilder();
     flow.add('quantum-circuit', 'n0', ['n1'], {structure: 'qubits', outputs: '1', qbitsreg: '1', cbitsreg: '1'});
     flow.add('hadamard-gate', 'n1', ['n2']);
     flow.add('measure', 'n2', ['n3'], {selectedBit: '0'});
     flow.add('local-simulator', 'n3', ['n4'], {shots: '1'});
     flow.addOutput('n4');
 
-    nodeTestHelper.load(flow.nodes, flow.flow, function() {
-      let n0 = nodeTestHelper.getNode('n0');
-      let n4 = nodeTestHelper.getNode('n4');
-
-      n4.on('input', function(msg) {
-        try {
-          let command = util.format(snippets.LOCAL_SIMULATOR, '1');
-          assert.strictEqual(shell.lastCommand, command);
-          done();
-        } catch (err) {
-          done(err);
-        }
-      });
-
-      n0.receive({payload: ''});
-    });
+    testUtil.commandExecuted(flow, 'n0', 'n4', command, done);
   });
 });
--- test/nodes/measure_spec.js
+++ test/nodes/measure_spec.js
@@ -1,6 +1,9 @@
-const measureNode = require('../../quantum/nodes/measure/measure.js');
+const util = require('util');
 const testUtil = require('../test-util');
 const nodeTestHelper = testUtil.nodeTestHelper;
+const {FlowBuilder} = require('../flow-builder');
+const measureNode = require('../../quantum/nodes/measure/measure.js');
+const snippets = require('../../quantum/snippets.js');
 
 
 describe('MeasureNode', function() {
@@ -15,5 +18,16 @@ describe('MeasureNode', function() {
 
   it('load node', function(done) {
     testUtil.isLoaded(measureNode, 'measure', done);
+  });
+
+  it('execute command', function(done) {
+    let command = util.format(snippets.MEASURE, '0, 0');
+    let flow = new FlowBuilder();
+    flow.add('quantum-circuit', 'n0', ['n1'], {structure: 'qubits', outputs: '1', qbitsreg: '1', cbitsreg: '1'});
+    flow.add('hadamard-gate', 'n1', ['n2']);
+    flow.add('measure', 'n2', ['n3'], {selectedBit: '0'});
+    flow.addOutput('n3');
+
+    testUtil.commandExecuted(flow, 'n0', 'n3', command, done);
   });
 });
