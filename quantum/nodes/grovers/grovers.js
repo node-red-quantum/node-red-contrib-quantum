ac0decd5d340e104f36cbe2c77a7ed76a11b8581
Louis Lefevre <louislefev@gmail.com>
2021-08-18 16:39:08 +0100

    Remove some redundant variables.
    - Name is not used in the code and can be removed.
    - Removed results constant, since we can just overrwrite 'data' here.
    - Add a missing comma.
    - Use longhand structure for conditional (dont want to combine shorthand
      with longhand format).

--- quantum/nodes/grovers/grovers.js
+++ quantum/nodes/grovers/grovers.js
@@ -9,7 +9,6 @@ const shell = new PythonShellClass();
 module.exports = function(RED) {
   function GroversNode(config) {
     RED.nodes.createNode(this, config);
-    this.name = config.name || 'Grovers';
 
     this.on('input', async function(msg, send, done) {
       let error = errors.validateGroversInput(msg);
@@ -21,12 +20,13 @@ module.exports = function(RED) {
       const script = util.format(snippets.GROVERS, msg.payload);
       await shell.start();
       await shell.execute(script, (err, data) => {
-        if (err) done(err);
-        else {
-          const results = data.split('\n');
+        if (err) {
+          done(err);
+        } else {
+          data = data.split('\n');
           msg.payload = {
-            topMeasurement: results[0],
-            iterationsNum: Number(results[1])
+            topMeasurement: data[0],
+            iterationsNum: Number(data[1]),
           };
           send(msg);
           done();
