8f560f9d94222042ca6ce4fd2a2ce954795450fe
Louis Lefevre <louislefev@gmail.com>
2021-08-18 23:50:36 +0100

    Fix PythonShell tests not starting fresh

--- test/python_spec.js
+++ test/python_spec.js
@@ -9,6 +9,10 @@ NameError: name 'x' is not defined`;
 
 
 describe('PythonShell', function() {
+  before(() => {
+    shell.stop();
+  });
+
   describe('#constructor', function() {
     it('python path is default', async function() {
       assert.match(shell.path, /venv\/bin\/python|venv\/Scripts\/python.exe/);
