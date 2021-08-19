001f539b7e9fb7a45b98e0cfee1390ca086a7624
zhiqiang <zhiqiang@zhiqiangdeMacBook-Pro.local>
2021-08-19 17:40:45 +0800

    modify output format

--- quantum/nodes/shors/shors.js
+++ quantum/nodes/shors/shors.js
@@ -3,29 +3,30 @@
 const util = require('util');
 const snippets = require('../../snippets');
 const errors = require('../../errors');
-const {PythonShellClass} = require('../../python');
-const shell = new PythonShellClass();
+const shell = require('../../python').PythonShell;
 
 module.exports = function(RED) {
   function ShorsNode(config) {
     RED.nodes.createNode(this, config);
-    this.name = config.name || 'Shors';
+    this.name = config.name;
+    this.integer = config.integer;
+    const node = this;
 
     this.on('input', async function(msg, send, done) {
-      // let error = errors.validateGroversInput(msg);
+      // let error = errors.validateShorsInput(msg);
       // if (error) {
       //   done(error);
       //   return;
       // }
-      const script = util.format(snippets.SHORS, msg.payload);
+      const params = node.integer;
+      const script = util.format(snippets.SHORS, params);
       await shell.start();
       await shell.execute(script, (err, data) => {
         if (err) {
           done(err);
         } else {
-          // data = data.split('\n');
           msg.payload = {
-            listOfFactors: data[0],
+            listOfFactors: data,
           };
           send(msg);
           done();
