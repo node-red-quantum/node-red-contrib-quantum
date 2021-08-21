d9d974c9233b63641d74422c89d606630a2171c5
Khesim Reid <khesim.reid@gmail.com>
2021-08-21 12:42:49 +0100

    Test for qubits passing through phase gate.

--- test/nodes/phase-gate_spec.js
+++ test/nodes/phase-gate_spec.js
@@ -1,7 +1,10 @@
 const phaseGateNode = require('../../quantum/nodes/phase-gate/phase-gate.js');
 const testUtil = require('../test-util');
 const nodeTestHelper = testUtil.nodeTestHelper;
+const shell = require('../../quantum/python.js').PythonShell;
+const {FlowBuilder} = require('../flow-builder.js');
 
+const flow = new FlowBuilder();
 
 describe('PhaseGateNode', function() {
   beforeEach(function(done) {
@@ -9,11 +12,27 @@ describe('PhaseGateNode', function() {
   });
 
   afterEach(function(done) {
+    shell.stop();
+    flow.reset();
     nodeTestHelper.unload();
     nodeTestHelper.stopServer(done);
   });
 
   it('load node', function(done) {
     testUtil.isLoaded(phaseGateNode, 'phase-gate', done);
+  });
+
+  it('pass qubit through gate', function(done) {
+    flow.add('quantum-circuit', 'n0', ['n1'], {structure: 'qubits', outputs: '1', qbitsreg: '1', cbitsreg: '1'});
+    flow.add('phase-gate', 'n1', ['n2'], {phase: '1'});
+    flow.addOutput('n2');
+
+    let payloadObject = {
+      structure: {qubits: 1, cbits: 1},
+      register: undefined,
+      qubit: 0,
+    };
+
+    testUtil.qubitsPassedThroughGate(flow, payloadObject, done);
   });
 });
