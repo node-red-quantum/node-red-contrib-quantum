a585a5ba89979173c3607e07103385cfb766e1e3
Louis Lefevre <louislefev@gmail.com>
2021-08-23 10:59:42 +0100

    Louis/command-tests (#88)
    * Add commandExecuted test util function
    
    Function for testing whether a command has been executed by a node.
    
    * Add execute script tests for measure and simulator
    
    * Add inputId() and outputId() methods
    
    These methods can be used to easily retrieve the IDs of the first and
    last nodes. Updated the commandExecuted() function to reflect this
    change.
    
    * Fix multiple nodes of the same type being added
    
    * Remove assumed wires array
    
    Remove wires being put into an array in add() method. This causes
    complications if a node is connected to multiple other nodes.
    
    * Fix PythonShell tests not starting fresh
    
    * Add execute command tests
    
    Includes:
    - Bloch sphere
    - Circuit diagram
    - Classical register
    - Hadamard gate
    - Quantum circuit
    - Quantum register
    - Toffoli gate
    
    * Run Node.js CI workflow on test/ file
    
    * Revert "Fix PythonShell tests not starting fresh"
    
    This reverts commit 8f560f9d94222042ca6ce4fd2a2ce954795450fe.
    
    * Increase timeout limit
    
    * Add before hook to PythonShell constructor tests
    
    Stops the shell before running tests.
    
    * Fix done() being called multiple times
    
    * Add Toffoli gate command executed test
    
    * Add multi-controlled-u-gate test suite
    
    * Fix multiple done calls on error
    
    * Add ibm-quantum-system execute command test
    
    This command is disabled by default as it requires an API key to run.
    For this to run on the CI, GitHub secrets could be used. To run this
    locally, an API key should be assigned to the API_TOKEN constant.
    
    * Add more execute command tests
    
    Add tests for:
    - barrier_spec.js
    - cnot-gate_spec.js
    - controlled-u-gate_spec.js
    - identity-gate_spec.js
    - multi-controlled-u-gate_spec.js
    - not-gate_spec.js
    - phase-gate_spec.js
    - reset_spec.js
    - rotation-gate_spec.js
    - swap_spec.js
    - unitary-gate_spec.js

--- test/nodes/bloch-sphere_spec.js
+++ test/nodes/bloch-sphere_spec.js
@@ -1,6 +1,8 @@
-const blochSphereNode = require('../../quantum/nodes/bloch-sphere/bloch-sphere.js');
 const testUtil = require('../test-util');
 const nodeTestHelper = testUtil.nodeTestHelper;
+const {FlowBuilder} = require('../flow-builder');
+const blochSphereNode = require('../../quantum/nodes/bloch-sphere/bloch-sphere.js');
+const snippets = require('../../quantum/snippets.js');
 
 
 describe('BlochSphereNode', function() {
@@ -15,5 +17,20 @@ describe('BlochSphereNode', function() {
 
   it('load node', function(done) {
     testUtil.isLoaded(blochSphereNode, 'bloch-sphere', done);
+  });
+
+  it('execute command', function(done) {
+    let command = snippets.BLOCH_SPHERE + snippets.ENCODE_IMAGE;
+    let flow = new FlowBuilder();
+    flow.add('quantum-circuit', 'n0', [['n1'], ['n2'], ['n3']],
+        {structure: 'qubits', outputs: '3', qbitsreg: '3', cbitsreg: '1'});
+    flow.add('hadamard-gate', 'n1', [['n4']]);
+    flow.add('hadamard-gate', 'n2', [['n4']]);
+    flow.add('not-gate', 'n3', [['n4']]);
+    flow.add('toffoli-gate', 'n4', [['n5'], ['n5'], ['n5']], {targetPosition: 'Middle'});
+    flow.add('bloch-sphere', 'n5', [['n6']]);
+    flow.addOutput('n6');
+
+    testUtil.commandExecuted(flow, command, done);
   });
 });
