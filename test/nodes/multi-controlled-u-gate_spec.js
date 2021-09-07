0ffbcedec0cef6eccba4353c1c44da1f28313402
Timur Toktassynov <timur.toktassynov@gmail.com>
2021-09-07 22:06:00 +0100

    fixed this failing Pass qubit through gate failing test and added correct output test

--- test/nodes/multi-controlled-u-gate_spec.js
+++ test/nodes/multi-controlled-u-gate_spec.js
@@ -23,12 +23,12 @@ describe('MultiControlledUGateNode', function() {
     testUtil.isLoaded(multiControlledUGateNode, 'multi-controlled-u-gate', done);
   });
 
-  xit('pass qubit through gate', function(done) {
-    flow.add('quantum-circuit', 'n0', [['n1'], ['n1'], ['n1']],
+  it('pass qubit through gate', function(done) {
+    flow.add('quantum-circuit', 'qc', [['mc'], ['mc'], ['mc']],
         {structure: 'qubits', outputs: '3', qbitsreg: '3', cbitsreg: '3'});
-    flow.add('multi-controlled-u-gate', 'n1', [['n2'], ['n2'], ['n2']],
-        {nbControls: '3', targetPosition: '2', theta: '0', phi: '0', lambda: '0', gamma: '0'});
-    flow.addOutput('n2');
+    flow.add('multi-controlled-u-gate', 'mc', [['out'], ['out'], ['out']],
+        {outputs: 3, nbControls: 2, targetPosition: 1, theta: '0', phi: '0', lambda: '0', gamma: '0'});
+    flow.addOutput('out');
 
     let payloadObject = [
       {structure: {qubits: 3, cbits: 3},
@@ -76,5 +76,22 @@ describe('MultiControlledUGateNode', function() {
     const givenInput = {payload: {structure: '', qubit: 3}, topic: 'Quantum Circuit'};
     const expectedMessage = errors.NOT_QUBIT_OBJECT;
     testUtil.nodeFailed(flow, givenInput, expectedMessage, done);
+  });
+
+  it('should return correct output', function(done) {
+    flow.add('quantum-circuit', 'qc', [['qr'], ['cr']],
+      {structure: 'registers', outputs: '2', qbitsreg: '1', cbitsreg: '1'});
+    flow.add('quantum-register', 'qr', [['mcu'], ['mcu']], {outputs: 2});
+    flow.add('classical-register', 'cr', [], {classicalBits: '2'});
+    flow.add('multi-controlled-u-gate', 'mcu', [['m1'], ['m2']],
+      {outputs: 2, nbControls: 1, targetPosition: '0', theta: '0', phi: '0', lambda: '0'});
+    flow.add('measure', 'm1', [['si']], {selectedBit: '0'});
+    flow.add('measure', 'm2', [['si']], {selectedBit: '1'});
+    flow.add('local-simulator', 'si', [['out']], {shots: '1'});
+    flow.addOutput('out');
+
+    const givenInput = {payload: ''};
+    const expectedOutput = {'00': 1};
+    testUtil.correctOutputReceived(flow, givenInput, expectedOutput, done);
   });
 });
