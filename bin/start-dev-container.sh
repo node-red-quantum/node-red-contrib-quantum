578963d0e6f5fe4abc9e5606aa36d77e11407355
Robin Zhou <bbaayezi@gmail.com>
2021-09-10 07:21:33 +0100

    Rui/docker support (#54)
    * add docker-based dev environment support
    
    * enable hot reload
    
    * fix running existing dev container
    
    * install dependencies in container
    
    * run npm install before starting node-red
    
    * update Dockerfile.dev
    
    * modify script to be used as entrypoint of Dockerfile.dev
    
    * remove npm command 'start:dev'
    
    * add Dockerfile.prod
    
    * add docker-compose file to somplify the process of starting development container
    
    * update restart policy of dev container
    
    * update README
    
    * Update README.md
    
    - Add missing TOC heading.
    - Fix some spelling/grammar.
    - Shorten Docker heading for brevity.
    
    * Update start-dev-container.sh shebang
    
    Use environment Bash for better reliability.
    
    [skip ci]
    
    * apply suggested change
    
    Co-authored-by: Louis Lefevre <louislefev@gmail.com>

---
+++ bin/start-dev-container.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Install the quantum nodes
+cd /root/.node-red && \
+npm install /node-red-contrib-quantum && \
+# Run development server using nodemon to enable hot reloading
+cd /node-red-contrib-quantum && \
+nodemon --exec node-red flows_quantum.json
